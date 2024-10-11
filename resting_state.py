9aacdf3ad4e03e704ba255c892d6fe08f4715483
mh105 <alexhe0218@gmail.com>
2024-10-10 19:34:49 -0700

    BF: remove unused routines

--- resting_state.py
+++ resting_state.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.2a1),
-    on Thu Oct 10 14:45:31 2024
+    on Thu Oct 10 19:33:50 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
